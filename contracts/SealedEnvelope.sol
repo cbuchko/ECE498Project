7b2944b770dfbe0b20b85e547a78eb1351770f91
Noah Clarke <noahwclarke@uvic.ca>
2022-07-23 11:37:25 -0700

    update

--- contracts/SealedEnvelope.sol
+++ contracts/SealedEnvelope.sol
@@ -15,7 +15,7 @@ contract SealedEnvelope {
     address seller_addr;
     address contract_addr;
     uint256 reveal_time;
-    uint256 finish_time;
+    bool finished;
     uint256 start_time;
     uint256 public auction_item;
 
@@ -31,21 +31,24 @@ contract SealedEnvelope {
 
         reveal_time = block.timestamp + bid_interval;
         start_time = block.timestamp;
-        finish_time = block.timestamp + bid_interval + post_bid_interval;
+        finished = false;
     }
 
     // Add a bid and place the bid amount in escrow
-    function PlaceBid(bytes32 bid) public isBidder payable {
+    function PlaceBid(bytes32 bid) public payable isBidder {
         require(
             start_time <= block.timestamp && block.timestamp <= finish_time
         );
 
-        balance[msg.sender] = msg.value
-
-        
+        balance[msg.sender] = msg.value;
     }
 
-    function handleWithdraw() public isSeller payable returns (uint256) {
+    function TallyBids() public inBidding returns () {
+        uint length = bids[]
+        return;
+    }
+
+    function handleTransfer() public payable returns (uint256) {
         uint256 new_balance = balance[msg.sender];
         if (new_balance > 0) {
             balance[msg.sender] = 0;
@@ -55,32 +58,32 @@ contract SealedEnvelope {
     }
 
     //send auction item to winner and take funds
-    function WithdrawWinningBid() public isSeller inRevealing returns (uint256) {
+    function awardWinningBid() public isWinner inRevealing returns () {
         ContractInterface(contract_addr).sendToken(auction_item, msg.sender);
-        uint256 new_balance = handleWithdraw();
+        uint256 new_balance = handleTransfer();
         return new_balance;
     }
 
     //return funds to loser
-    function WithdrawLosingBid() public isSeller inRevealing returns (uint256) {
-        uint256 new_balance = handleWithdraw();
+    function WithdrawLosingBid() public isLoser inRevealing returns (uint256) {
+        uint256 new_balance = handleTransfer();
         return new_balance;
     }
 
     // Pays the seller of the auction item the amount
     function PaySeller() public isSeller inRevealing returns (uint256) {
         ContractInterface(contract_addr).sendToken(auction_item, msg.sender);
-        uint256 new_balance = handleWithdraw();
+        uint256 new_balance = handleTransfer();
         return new_balance;
     }
 
     // Determine the winner of the auction
-    function RevealWinner()  public isSeller inRevealing {
+    function RevealWinner() public isSeller inRevealing {
         require(reveal_time <= block.timestamp);
     }
 
     // Return losing bids and auction_item to winning bidder
-    function FinishAuction()  public isSeller inRevealing {}
+    function FinishAuction() public isSeller inRevealing {}
 
     modifier inBidding() {
         require(
@@ -97,10 +100,18 @@ contract SealedEnvelope {
     }
 
     modifier isSeller() {
-        require(msg.sender == seller_addr)
+        require(msg.sender == seller_addr);
     }
 
     modifier isBidder() {
-        require(msg.sender != seller_addr)
+        require(msg.sender != seller_addr);
+    }
+
+    modifier isWinner() {
+        require(msg.sender == winner_addr && msg.sender != seller_addr);
+    }
+
+    modifier isLoser() {
+        require(msg.sender != winner_addr && msg.sender != seller_addr);
     }
 }
